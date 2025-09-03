bf3dcf3b58c5b32863056fa0705881d24ff3aa5a
jameskedeveloper <jamestechwap@gmail.com>
2025-09-03 12:02:19 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,25 @@
+/*
+┏━━━━━━━━━━━━━━━┓
+┃     𝐉𝐀𝐌𝐄𝐒𝐓𝐄𝐂𝐇
+┣━━━━━━━━━━━━━━━┛
+┃whatsapp : +254785016388
+┃owner : james
+┃base : vimpire killer 
+┃best friend : ibrahim / trashcore dev
+┃helper : my brain😂😂
+┃maintainer : james
+┃deals : t.me/jamespydev
+┃pterodactyl hosting buy from james dev
+┗━━━━━━━━━━━━━━━┛
+*/
+global.prefix = [".", "!", ".", ",", "🐤", "🗿"]; 
+global.publik = true
+global.owner = ["254785016388"] 
+global.namabot = 'terminator'
+//======================
+global.mess = { 
+owner: 'waduhh!, lu bukan owner gw bg',
+premium: 'anda bukan user premium',
+succes: 'done bang'
+}
+//======================
